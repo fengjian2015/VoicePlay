578bd69444b8e78a3ce9b7ef645e87e70358385f
feng <fengjiannattiness@163.com>
2023-03-30 09:39:22 +0800

    优化音频播放

--- voicelibrary/src/main/java/com/lucksoft/luckvoice/lib/VoicePlay.java
+++ voicelibrary/src/main/java/com/lucksoft/luckvoice/lib/VoicePlay.java
@@ -139,37 +139,49 @@ public class VoicePlay {
                         //异步需要等待加载完成，音频才能播放成功
                         soundPool.setOnLoadCompleteListener((soundPool1, sampleId, status) -> {
                             Log.d("VoicePlay", "status:"+status);
-                            if (status == 0) {
-                                Log.d("VoicePlay", "开始播放"+Thread.currentThread());
-                                //第一个参数soundID
-                                //第二个参数leftVolume为左侧音量值（范围= 0.0到1.0）
-                                //第三个参数rightVolume为右的音量值（范围= 0.0到1.0）
-                                //第四个参数priority 为流的优先级，值越大优先级高，影响当同时播放数量超出了最大支持数时SoundPool对该流的处理
-                                //第五个参数loop 为音频重复播放次数，0为值播放一次，-1为无限循环，其他值为播放loop+1次
-                                //第六个参数 rate为播放的速率，范围0.5-2.0(0.5为一半速率，1.0为正常速率，2.0为两倍速率)
-                                if (isFistLoad.get()){
-                                    isFistLoad.set(false);
-                                    mExecutorService.execute(() -> {
-                                        for (int i=0 ;i<soundIdSet.size();i++){
-                                            int soundId = soundIdSet.get(i);
-                                            long delaytime = delayTimeList.get(i) ;
-
-                                            soundPool1.play(soundId, 1, 1, 0, 0, 1);
+                            mExecutorService.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //这里不要使用数量来对比是否初始化完毕，因为部分机型不按常理出牌
+                                    if (status == 0) {
+                                        Log.d("VoicePlay", "开始播放"+Thread.currentThread());
+                                        //第一个参数soundID
+                                        //第二个参数leftVolume为左侧音量值（范围= 0.0到1.0）
+                                        //第三个参数rightVolume为右的音量值（范围= 0.0到1.0）
+                                        //第四个参数priority 为流的优先级，值越大优先级高，影响当同时播放数量超出了最大支持数时SoundPool对该流的处理
+                                        //第五个参数loop 为音频重复播放次数，0为值播放一次，-1为无限循环，其他值为播放loop+1次
+                                        //第六个参数 rate为播放的速率，范围0.5-2.0(0.5为一半速率，1.0为正常速率，2.0为两倍速率)
+                                        if (isFistLoad.get()){
+                                            isFistLoad.set(false);
                                             try {
-                                                Log.d("VoicePlay", "开始播放--- 休眠时长" + delaytime + "  "+Thread.currentThread());
+                                                Log.d("VoicePlay", "暂时休眠，避免音频未初始化完毕");
                                                 //获取当前音频的时长
-                                                Thread.sleep(delaytime);
+                                                Thread.sleep(100);
                                             } catch (InterruptedException e) {
                                                 e.printStackTrace();
                                             }
+                                            for (int i=0 ;i<soundIdSet.size();i++){
+                                                int soundId = soundIdSet.get(i);
+                                                long delaytime = delayTimeList.get(i) ;
+
+                                                soundPool1.play(soundId, 1, 1, 0, 0, 1);
+                                                try {
+                                                    Log.d("VoicePlay", "开始播放--- 休眠时长" + delaytime + "  "+Thread.currentThread());
+                                                    //获取当前音频的时长
+                                                    Thread.sleep(delaytime);
+                                                } catch (InterruptedException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+                                            soundPool1.release();
+                                            mCountDownLatch.countDown();
                                         }
-                                        soundPool1.release();
+                                    }else{
                                         mCountDownLatch.countDown();
-                                    });
+                                    }
                                 }
-                            }else{
-                                mCountDownLatch.countDown();
-                            }
+                            });
+
                         });
                         return true;
                     }
